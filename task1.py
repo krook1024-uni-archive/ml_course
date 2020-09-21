818a4c49894070bcf9a9982fa004edd604ec5355
Molnár Antal <krook1024@gmail.com>
2020-09-21 15:33:09 +0200

    task1

---
+++ task1.py
@@ -0,0 +1,43 @@
+# generált adatok
+
+# n        - rekordok száma
+# B_0      = 3
+# B_1      = 2
+# sig^2    = 0.1
+# X        ~ normális (0,1)
+# eps      ~ normális (0, \sigma^2)
+# y        = B_0 + B_1 X x + eps
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model, model_selection
+
+# random adatok
+n = 100
+b0 = 3
+b1 = 2
+sig = 0.5
+X = np.random.normal(size=[n, 1])
+eps = np.random.normal(scale=sig, size=[n, 1])
+y = b0 + b1 * X + eps
+
+# lineáris regresszió
+reg = linear_model.LinearRegression()
+reg.fit(X, y)
+intercept = reg.intercept_ # should match b0
+coef = reg.coef_ # should match b1
+score = reg.score(X, y)
+X_test_pred = reg.predict(X)
+y_test_pred = reg.predict(y)
+
+# pontdiagram
+plt.figure(1)
+plt.title('Pontdiagram regressziós egyenessel')
+plt.xlabel('x input')
+plt.ylabel('y input')
+plt.scatter(X, y, color="blue")
+xrange = [np.amin(X) - 0.1, np.amax(X) + 0.1]
+yrange = [np.amin(y) - 0.1, np.amax(y) + 0.1]
+plt.plot(xrange, yrange, color='red')
+plt.plot(y, y_test_pred, color='green')
+plt.show()
